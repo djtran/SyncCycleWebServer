75f49d90170941eb9166d6979e246e3da872c416
djtran <djtran@bu.edu>
2017-02-13 22:39:13 -0500

    Updated the scGPIO.js to have rudimentary debouncing and also to average the last 5 speeds together when calculating what is stored in the database.

--- Hardware to Database/scGPIO.js
+++ Hardware to Database/scGPIO.js
@@ -7,6 +7,9 @@ var Gpio = require('onoff').Gpio,
 
 var db;
 var beginningMillis;
+var lastTime;
+
+var lastSpeeds = [0,0,0,0,0];
 
 MongoCycle.openMongo(function(database){
 	db = database;
@@ -14,6 +17,7 @@ MongoCycle.openMongo(function(database){
 	MongoCycle.createRide(db, new Date(), 1, function(collection){
 
 		beginningMillis = Date.now();
+		lastTime = beginningMillis;
 
 		speedometer.watch(function(err, value){
 			if(err)
@@ -22,15 +26,51 @@ MongoCycle.openMongo(function(database){
 			}
 			else
 			{
-				var time = Date.now() - beginningMillis;
-				console.log("Adding t: " + time + " v: " + value + " to db");
-				MongoCycle.addDataPoint(collection, MongoCycle.Sensor.speedometer, time, value);
+				//debouncing. If you got another tick in the last .1 seconds, disregard it.
+				if(Date.now() - lastTime < 100)
+				{
+					return;
+				}
+				else
+				{
+					lastTime = Date.now();
+					var time = lastTime - beginningMillis;
+
+					//We have a 26x1.75 wheel : Circumference = 2023 mm
+					//speed = distance / time (mm/ms = meters / second)
+					//2.23694 mi/h = 1 m/s
+					var speed = (2023/time)/2.23694;
+
+					speed = updateSpeed(speed);
+
+					console.log("Adding t: " + time + " v: " + speed + " to db");
+					MongoCycle.addDataPoint(collection, MongoCycle.Sensor.speedometer, time, speed);
+				}
 			}
 		})
 
 	});
 });
 
+function updateSpeed(value){
+	if(lastSpeeds.length > 5)
+	{
+		lastSpeeds.shift();
+	}
+
+	lastSpeeds.push(value);
+
+	var calculate = 0;
+	for(var i = lastSpeeds.length - 1; i >= 0; i--)
+	{
+		calculate += ((i+1)/5)*lastSpeeds[i];
+	}	
+
+	// (1/5 + 2/5 + 3/5 + 4/5 + 5/5) = 3
+	calculate = calculate/3;
+
+	return calculate;
+}
 
 process.on('SIGINT', function () {
   speedometer.unexport();
